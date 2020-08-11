1face2a8d985342d19c84861d4c971b2e997da90
Project-sarvagya <tech.sarvagya@yahoo.com>
2020-08-11 13:46:35 +0530

    First Commit

---
+++ js/custom.js
@@ -0,0 +1,65 @@
+    new WOW().init();
+
+(function ($) {
+
+   'use strict';
+
+    /*
+     * ----------------------------------------------------------------------------------------
+     *  SMOTH SCROOL JS
+     * ----------------------------------------------------------------------------------------
+     */
+
+    $('a.smoth-scroll').on('click', function (e) {
+        var anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $(anchor.attr('href')).offset().top - 50
+        }, 1000);
+        e.preventDefault();
+    });
+
+
+
+    /* ==========================================================================
+      COUNTER UP 
+ ========================================================================== */
+
+
+    $('.carousel').carousel({
+      interval: 8000
+    });
+    
+    /* Closes the Responsive Menu on Menu Item Click*/
+    $('.navbar-collapse .navbar-nav a').on('click', function () {
+        $('.navbar-toggler:visible').click();
+    });
+    /*END MENU JS*/
+
+    
+    /* ----------------------------------------------------------- */
+    /*  Fixed header
+    /* ----------------------------------------------------------- */
+
+
+    $(window).on('scroll', function () {
+        if ($(window).scrollTop() > 70) {
+            $('.site-navigation,.trans-navigation').addClass('header-white');
+        } else {
+            $('.site-navigation,.trans-navigation').removeClass('header-white');
+        }
+
+
+
+    });
+
+
+    /* ==========================================================================
+      SCROLL SPY
+ ========================================================================== */
+
+    $('body').scrollspy({
+        target: '.navbar-collapse',
+        offset: 195
+    });
+
+})(window.jQuery);
